70db37c8db77e7324b3c7310bac30bf1caa0473e
KRISHNA KARTHIK KADAPA <karthik@Karthik.local>
2016-02-15 13:35:53 -0800

    removed unwantedcode from model

--- src/main/java/com/imageview/Main.java
+++ src/main/java/com/imageview/Main.java
@@ -7,11 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Model model = new Model();
-
-        model.setSameValueCount(1);
-        model.setWordOccurenceInFiles(4609);
-
         App app = new App();
         String string = app.getTextData("Project");
 
